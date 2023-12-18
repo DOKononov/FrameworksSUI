25eac8a18ca12cd34451939d8c0c65afed908275
d.o.kononov <d.o.kononov@gmail.com>
2023-12-18 19:13:31 +0300

    Initial Commit

---
+++ FrameworksSUI/FrameworksSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksSUIApp.swift
+//  FrameworksSUI
+//
+//  Created by Dmitry Kononov on 18.12.23.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
